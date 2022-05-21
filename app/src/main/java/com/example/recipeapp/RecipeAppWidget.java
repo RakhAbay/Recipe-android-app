b7efb6c677a91fd733b3ae34f8944993ce5b93ad
Rakhman <iwillshowyou.2017@gmail.com>
2022-05-21 21:13:27 +0600

    feat: added button text change ability

--- app/src/main/java/com/example/recipeapp/RecipeAppWidget.java
+++ app/src/main/java/com/example/recipeapp/RecipeAppWidget.java
@@ -1,10 +1,14 @@
 package com.example.recipeapp;
 
+import static com.example.recipeapp.WidgetConfig.KEY_BUTTON_TEXT;
+import static com.example.recipeapp.WidgetConfig.SHARED_PREFS;
+
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.widget.RemoteViews;
 
 import com.example.recipeapp.activites.Favorites.FavoritesActivity;
@@ -17,14 +21,18 @@ public class RecipeAppWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-        Intent intent = new Intent(context, RecipeDetailsActivity.class);
-        intent.putExtra("recipeId", (long)716426);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            Intent intent = new Intent(context, RecipeDetailsActivity.class);
+            intent.putExtra("recipeId", (long)716426);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_app_widget);
-        views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+            SharedPreferences prefs = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+            String buttonText = prefs.getString(KEY_BUTTON_TEXT + appWidgetId, "Press me");
 
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_app_widget);
+            views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+            views.setCharSequence(R.id.widget_button, "setText", buttonText);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
     }
     }
 }
