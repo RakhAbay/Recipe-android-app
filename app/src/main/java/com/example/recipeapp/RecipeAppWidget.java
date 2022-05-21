72ea6cb42a1db140f9322935cdd402352d344b7a
Rakhman <iwillshowyou.2017@gmail.com>
2022-05-21 21:23:27 +0600

    feat: make resizble widget

--- app/src/main/java/com/example/recipeapp/RecipeAppWidget.java
+++ app/src/main/java/com/example/recipeapp/RecipeAppWidget.java
@@ -9,7 +9,10 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.RemoteViews;
+import android.widget.Toast;
 
 import com.example.recipeapp.activites.Favorites.FavoritesActivity;
 import com.example.recipeapp.activites.RecipeDetails.RecipeDetailsActivity;
@@ -32,7 +35,47 @@ public class RecipeAppWidget extends AppWidgetProvider {
             views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
             views.setCharSequence(R.id.widget_button, "setText", buttonText);
 
+            Bundle appWidgetOptions = appWidgetManager.getAppWidgetOptions(appWidgetId);
+            resizeWidget(appWidgetOptions, views);
+
             appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
     }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_app_widget);
+
+        resizeWidget(newOptions, views);
+
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    private void resizeWidget(Bundle appWidgetOptions, RemoteViews views) {
+        int minWidth = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+        int maxWidth = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH);
+        int minHeight = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_HEIGHT);
+        int maxHeight = appWidgetOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_HEIGHT);
+
+        if (maxHeight > 100) {
+            views.setViewVisibility(R.id.widget_text, View.VISIBLE);
+        } else {
+            views.setViewVisibility(R.id.widget_text, View.GONE);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        Toast.makeText(context, "onDeleted", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        Toast.makeText(context, "onEnabled", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        Toast.makeText(context, "onDisabled", Toast.LENGTH_SHORT).show();
     }
 }
