c5f1fb607f95e4fb82ed74a199896c89fc62c82d
Rakhman <iwillshowyou.2017@gmail.com>
2022-05-21 20:29:42 +0600

    feat: added button to widget

--- app/src/main/java/com/example/recipeapp/RecipeAppWidget.java
+++ app/src/main/java/com/example/recipeapp/RecipeAppWidget.java
@@ -1,42 +1,30 @@
 package com.example.recipeapp;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.example.recipeapp.activites.Favorites.FavoritesActivity;
+import com.example.recipeapp.activites.RecipeDetails.RecipeDetailsActivity;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class RecipeAppWidget extends AppWidgetProvider {
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-    }
-
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
-        }
-    }
+        Intent intent = new Intent(context, RecipeDetailsActivity.class);
+        intent.putExtra("recipeId", (long)716426);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
-    @Override
-    public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
-    }
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.recipe_app_widget);
+        views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
 
-    @Override
-    public void onDisabled(Context context) {
-        // Enter relevant functionality for when the last widget is disabled
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
     }
 }
